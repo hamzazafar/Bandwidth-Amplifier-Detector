381fc6e2ede7469f8f0e0a5f001bead23517668f
Hamza Zafar <hamza.zafar1993@gmail.com>
2018-12-18 16:38:22 +0100

    add a dummy scan task

--- core/tasks.py
+++ core/tasks.py
@@ -2,13 +2,11 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 @shared_task
-def add(x, y):
-    print("Hi there!")
-    return x + y
-
-@shared_task
-def testing():
-    print("HELO")
-    return 100
+def scan(address_range, target_port, version):
+    logger.info('Address Range: {0}  UDP Port: {1}  IP Version: {2}'
+                .format(address_range, target_port, version))
--- core/views/scan_views.py
+++ core/views/scan_views.py
@@ -1,28 +1,14 @@
 from rest_framework import generics
 
-from core.models.scan import Scan
 from core.serializers.scan_serializers import ScanSerializer
 
 from django_celery_beat.models import PeriodicTask
 
-from rest_framework.response import Response
-
-from core.tasks import add
 
 class ScanListCreateView(generics.ListCreateAPIView):
-    #queryset  = Scan.objects.all()
     queryset  = PeriodicTask.objects.all()
     serializer_class = ScanSerializer
 
-    #def list(self, request):
-    #    res = add.delay(5,1)
-    #    print("%s" % res.ready())
-    #    queryset = self.get_queryset()
-    #    serializer = ScanSerializer(queryset, many=True)
-    #    return Response(serializer.data)
-
-
 class ScanRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    #queryset  = Scan.objects.all()
     queryset = PeriodicTask.objects.all()
     serializer_class = ScanSerializer
