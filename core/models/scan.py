f44d5fe16802a1250706a0e9049e49b761ef5310
Hamza Zafar <hamza.zafar1993@gmail.com>
2019-02-01 13:30:30 +0100

    add Response model

--- core/models/scan.py
+++ core/models/scan.py
@@ -22,14 +22,25 @@ class ScanTimeSeriesResult(models.Model):
                                       on_delete=models.CASCADE,
                                       related_name='time_series_results')
 
-
 class Amplifier(models.Model):
 
     address = models.CharField(null=False, max_length=255)
     total_response_size = models.IntegerField(null=False)
     amplification_factor = models.IntegerField(null=False)
+    unsolicited_response = models.BooleanField(default=False)
 
     # relations
     scan = models.ForeignKey(ScanTimeSeriesResult,
                              related_name='amplifiers',
                              on_delete=models.CASCADE)
+
+class Response(models.Model):
+
+    response_hex_data = models.TextField(null=False)
+    response_size = models.IntegerField(null=False)
+
+    #relations
+    amplifier = models.ForeignKey(Amplifier,
+                                  related_name="responses",
+                                  on_delete=models.CASCADE)
+
